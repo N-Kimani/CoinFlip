e0f04c8c3e52c27b730fcdb437128a927ad824e8
N-Kimani <kimaninathan17@gmail.com>
2025-07-02 10:35:02 +0300

    Coin flip with randomizer

--- src/app/app.ts
+++ src/app/app.ts
@@ -12,20 +12,20 @@ export class App {
   headUrl = 'https://media.geeksforgeeks.org/wp-content/uploads/20200916123059/SHalfDollarObverse2016head-300x300.jpg'
   tailUrl = 'https://media.geeksforgeeks.org/wp-content/uploads/20200916123125/tails-200x200.jpg'
   imgUrl = this.headUrl
-  isHead = true;
 
   count = 0
   heads = 0
   tails = 0
+  decision = 0
   coinFlip(){
     this.count = this.count += 1;
-    if(this.isHead){
-      this.isHead = false
+    this.decision = Math.floor(Math.random() * 2);
+    if(this.decision == 1){
       this.imgUrl = this.tailUrl
       this.tails += 1
       return this.imgUrl
-    } else{
- this.isHead = true
+    } 
+    else{
     this.imgUrl = this.headUrl
     this.heads += 1
     return this.imgUrl
